989538b64c58ff4ca227b9c60254066a97c987eb
Rupesh-2010 <rups1808@gmail.com>
2025-12-30 20:22:14 +0530

    SQL

--- Apna Clg/Apna_clg.sql
+++ Apna Clg/Apna_clg.sql
@@ -6,6 +6,7 @@ drop database temp1;		-- Delete the DB
 use Rupesh_SQL;
 
 create table List(
+
 	id int primary key,
     name varchar(50),
     age int not null
@@ -15,6 +16,42 @@ insert into student values (1, "Rupesh", 25);
 insert into student values (2, "ABC", 5);
 
 Select * from student;
+
+SHOW DATABASES;
+SHOW TABLES;
+
+INSERT INTO list
+(id, name, age) 		-- parameters as decided. this is imp.
+values
+(101, "R",2),
+(102, "D",9),
+(103, "F",8);
+select * from list;
+
+
+
+Create Table Emp (
+	id int,
+    salary int default 25000);
+    
+insert into Emp (id) values (101);
+select * from Emp;
+
+#1.12
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ Apna Clg/task_01.sql
@@ -0,0 +1,34 @@
+create database XYZ;
+
+USE XYZ;
+
+create table Emp_Info(
+	id int primary key,
+	name varchar(50),
+	salary int
+);
+select * from Emp_Info;
+
+insert into Emp_Info
+values
+(111, "Rupesh",200),
+(112, "Ritesh",100),
+(113, "Rajan",500);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
